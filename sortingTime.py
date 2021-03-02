668f043b528695a173a57369b889022ed99080e6
BrockBasil <bbasil@purdue.edu>
2021-03-02 01:56:31 -0500

    Added randomize_data function that reads integers from data.txt, randomizes the data, and writes it back to data.txt

--- sortingTime.py
+++ sortingTime.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 import time
+import random
 import insertionSort
 import quickSortFirst
 import mergeSort
 
+def randomize_data():
+	array = []
+	with open("data.txt", "r") as f:
+		for line in f:
+			array.append(int(line))
+	f.close()
+
+	random.shuffle(array)
+	
+	with open('data.txt', 'w') as f:
+		for num in array:
+			f.write("%i\n" % num)
+	f.close()
+
+	print("Data in data.txt randomized")
+
 def main():
+	randomize_data()
+
 	start = time.time() 
 	insertionSort.main()
 	end = time.time()
