5c146e6797789cf14d6cc6d92657139473ab491b
BrockBasil <bbasil@purdue.edu>
2021-03-01 22:08:13 -0500

    Updated sortingTime to include newly added merge sort algorithm

--- sortingTime.py
+++ sortingTime.py
@@ -2,12 +2,18 @@
 import time
 import insertionSort
 import quickSortFirst
+import mergeSort
 
 def main():
 	start = time.time() 
 	insertionSort.main()
 	end = time.time()
 	print("Insertion Sort: {:.5f} seconds".format(end - start))
+
+	start = time.time() 
+	mergeSort.main()
+	end = time.time()
+	print("Merge Sort: {:.5f} seconds".format(end - start))	
 
 	start = time.time() 
 	quickSortFirst.main()
